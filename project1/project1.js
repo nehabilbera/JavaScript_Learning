d91cbb7c5ce1696d850fcc04c648ca6ec18e84ae
Neha Gupta <nehabilbera@gmail.com>
2024-10-27 13:16:47 +0530

    Color Scheme Switcher

---
+++ project1/project1.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('.button')
+console.log(buttons)
+
+const body = document.querySelector('.canva')
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+
+        if(e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id
+        }
+    })
+})
